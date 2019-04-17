36ecedb76738a3981bcacef8646e8ec3d7b9e28c
hao <xuhaonju@gmail.com>
2019-04-17 14:47:11 +0200

    update

---
+++ codex_related/cytobre/fcsrev.py
@@ -0,0 +1,5 @@
+"""This is a package to manipulate FCS data cleaning
+    step 1, fcs data structure
+    step 2, manipulate the fcs data, data cleaning
+    step 3, interactive interface for data cleaning
+"""
