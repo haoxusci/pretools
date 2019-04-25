36bab180eb28db7057c8adda0297f52598e6ea70
hao <xuhaonju@gmail.com>
2019-04-25 14:57:31 +0200

    update

--- codex_related/nolancell/__init__.py
+++ codex_related/nolancell/__init__.py
@@ -1,3 +1,8 @@
 """Test nolanlab's cell paper strategy
     Reference: Deep Profiling of Mouse Splenic Architecture withCODEX Multiplexed Imaging
+    comment1: spatial spillover compensation
+        'we estimated the cell-to-cell signal spill coefficients based on the fraction of shared boundary between each pair of cell objects based on
+        the fraction of shared boundary between each pair of cell objects,
+        resulting in a banded matrix (most cells don't have any shared
+        boundaries)'.
 """
